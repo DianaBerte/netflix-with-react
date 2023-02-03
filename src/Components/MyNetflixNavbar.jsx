df3319abc8f96b3d1bd232eb087bd9c24260e80e
DianaBerte <dianaberte.go@gmail.com>
2023-02-03 15:09:51 +0100

    css tentative

--- src/Components/MyNetflixNavbar.jsx
+++ src/Components/MyNetflixNavbar.jsx
@@ -9,7 +9,7 @@ class MyNetflixNavbar extends Component {
         return (
             <>
                 <Navbar bg="dark" expand="lg" >
-                    <Navbar.Brand href="#home">Netflix</Navbar.Brand>
+                    <img style={{ width: 100 }} src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="" />
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
