8c53c78a5112c560c3783232a893b500a30953a7
DianaBerte <dianaberte.go@gmail.com>
2023-02-08 14:21:03 +0100

    Created component TVShows

---
+++ src/Components/TVShows.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class TVShows extends Component {
+    render() {
+        return(
+            <h1>TV Shows</h1>
+        )
+    }
+}
+
+export default TVShows
