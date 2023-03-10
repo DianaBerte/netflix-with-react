ddbda28d0ad9654702072d8421edce4df178985d
DianaBerte <dianaberte.go@gmail.com>
2023-03-10 16:28:11 +0100

    Update TVShows.jsx

--- src/Components/TVShows.jsx
+++ src/Components/TVShows.jsx
@@ -4,7 +4,10 @@ class TVShows extends Component {
 
     fetchMovies = async () => {
         try {
-        let response = await fetch('http://www.omdbapi.com/?apikey=921138b3&s=' + this.props.movieName)
+        let response = await fetch(
+            // 'http://www.omdbapi.com/?apikey=921138b3&s='
+            'http://localhost:3000/medias/1vhd9zwlf2h6hj4'
+            + this.props.movieName)
         if(response.ok){
             let data = await response.json();
             console.log(data);
